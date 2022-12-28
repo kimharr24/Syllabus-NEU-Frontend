494f96a3894893f146aaacdba4020d454842c85b
Harrison Kim <kim.harr@northeastern.edu>
2022-12-28 16:28:05 -0500

    feature: modified interfaces to include additional properties

---
+++ src/assets/Department.tsx
@@ -0,0 +1,1 @@
+export type Department = 'CS' | 'EECE' | 'ENGW' | 'PHYS' | 'MATH' | 'JRNL';
--- src/assets/Syllabus.tsx
+++ src/assets/Syllabus.tsx
@@ -1,7 +1,12 @@
 import { Professor } from './Professor';
+import { Department } from './Department';
 
 export interface Syllabus {
     id: number;
     professor: Professor;
+    department: Department;
+    courseNumber: number;
+    courseTitle: string;
+    semester: string;
     syllabusURL: string;
 }
