96fc861540fa15c046be685453ee9b07b08a6ee7
Harrison Kim <kim.harr@northeastern.edu>
2023-01-01 16:05:13 -0500

    feature: landing page started with wave svg

---
+++ src/components/HeroSearch/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Typography, Stack } from '@mui/material';
+
+const HeroSearch: React.FC = () => {
+    return (
+        <Stack>
+            <Typography variant='h2'>Syllabus NEU</Typography>
+        </Stack>
+    );
+};
+
+export default HeroSearch;
--- src/components/Landing/index.tsx
+++ src/components/Landing/index.tsx
@@ -1,8 +1,23 @@
-const Landing = () => {
+import React from 'react';
+import { Box } from '@mui/material';
+import HeroSearch from '../HeroSearch';
+import Waves from '../../assets/layered-waves.svg';
+
+const Landing: React.FC = () => {
     return (
-        <div>
-            <div>This is the landing page</div>
-        </div>
+        <Box sx={{ backgroundColor: '#001220' }}>
+            <div
+                style={{
+                    backgroundImage: `url(${Waves})`,
+                    aspectRatio: '960/300',
+                    width: '100%',
+                    backgroundRepeat: 'no-repeat',
+                    backgroundSize: 'cover',
+                    backgroundPosition: 'center',
+                }}>
+                <HeroSearch />
+            </div>
+        </Box>
     );
 };
 
