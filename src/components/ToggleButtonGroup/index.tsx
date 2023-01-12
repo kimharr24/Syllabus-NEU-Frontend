fd88c2d22b15b2a936fc4ab6d0fedf441a494e60
Harrison Kim <kim.harr@northeastern.edu>
2023-01-11 20:34:43 -0500

    feature: added state to control color of hero text and toggle button selections

--- src/components/ToggleButtonGroup/index.tsx
+++ src/components/ToggleButtonGroup/index.tsx
@@ -4,9 +4,17 @@ import { Stack } from '@mui/material';
 import SchoolOutlinedIcon from '@mui/icons-material/SchoolOutlined';
 import WorkOutlineOutlinedIcon from '@mui/icons-material/WorkOutlineOutlined';
 import BalanceOutlinedIcon from '@mui/icons-material/BalanceOutlined';
-import { PRIMARY_COLOR } from '../../utils/colors';
+import {
+    NEU_RED,
+    TOGGLE_BUTTON_GREEN,
+    TOGGLE_BUTTON_YELLOW,
+} from '../../utils/colors';
 import { ToggleButtonState } from '../../assets/ToggleButtonState';
 import ToggleButton from './ToggleButton';
+
+interface ToggleButtonGroupProps {
+    setSelectedColor: (color: string) => void;
+}
 
 const ButtonGroupContainer = styled(Stack)`
     && {
@@ -15,7 +23,9 @@ const ButtonGroupContainer = styled(Stack)`
     }
 `;
 
-const ToggleButtonGroup: React.FC = () => {
+const ToggleButtonGroup: React.FC<ToggleButtonGroupProps> = ({
+    setSelectedColor,
+}) => {
     const [toggleButtonState, setToggleButtonState] =
         useState<ToggleButtonState>(ToggleButtonState.NEU);
 
@@ -24,24 +34,36 @@ const ToggleButtonGroup: React.FC = () => {
             <ToggleButton
                 actualToggleButtonState={toggleButtonState}
                 expectedToggleButtonState={ToggleButtonState.NEU}
-                icon={<SchoolOutlinedIcon style={{ color: PRIMARY_COLOR }} />}
-                setToggleButtonState={setToggleButtonState}>
+                icon={<SchoolOutlinedIcon style={{ color: NEU_RED }} />}
+                color={NEU_RED}
+                setToggleButtonState={setToggleButtonState}
+                setSelectedColor={setSelectedColor}>
                 NEU
             </ToggleButton>
             <ToggleButton
                 actualToggleButtonState={toggleButtonState}
                 expectedToggleButtonState={ToggleButtonState.CPS}
                 icon={
-                    <WorkOutlineOutlinedIcon style={{ color: PRIMARY_COLOR }} />
+                    <WorkOutlineOutlinedIcon
+                        style={{ color: TOGGLE_BUTTON_YELLOW }}
+                    />
                 }
-                setToggleButtonState={setToggleButtonState}>
+                color={TOGGLE_BUTTON_YELLOW}
+                setToggleButtonState={setToggleButtonState}
+                setSelectedColor={setSelectedColor}>
                 CPS
             </ToggleButton>
             <ToggleButton
                 actualToggleButtonState={toggleButtonState}
                 expectedToggleButtonState={ToggleButtonState.LAW}
-                icon={<BalanceOutlinedIcon style={{ color: PRIMARY_COLOR }} />}
-                setToggleButtonState={setToggleButtonState}>
+                icon={
+                    <BalanceOutlinedIcon
+                        style={{ color: TOGGLE_BUTTON_GREEN }}
+                    />
+                }
+                color={TOGGLE_BUTTON_GREEN}
+                setToggleButtonState={setToggleButtonState}
+                setSelectedColor={setSelectedColor}>
                 LAW
             </ToggleButton>
         </ButtonGroupContainer>
