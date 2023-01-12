4b96fc32ccff4b5a440c8af0cbf7bd7106067cf4
Harrison Kim <kim.harr@northeastern.edu>
2023-01-12 16:57:59 -0500

    refactor: modified hero image to include more river water

---
+++ src/assets/AvailableSemesters.tsx
@@ -0,0 +1,26 @@
+/* eslint-disable import/prefer-default-export */
+import { Semester } from './Semester';
+
+// Replace with a dynamic approach of querying DynamoDB for available semesters
+export const AvailableSemesters: Semester[] = [
+    {
+        id: 0,
+        semester: 'Fall 2020',
+    },
+    {
+        id: 1,
+        semester: 'Spring 2021',
+    },
+    {
+        id: 2,
+        semester: 'Summer 1 2021',
+    },
+    {
+        id: 3,
+        semester: 'Summer 2 2021',
+    },
+    {
+        id: 4,
+        semester: 'Spring 2023',
+    },
+];
