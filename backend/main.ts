2d522b8ee2540769f32c448bddf80217edcc1727
Harrison Kim <kim.harr@northeastern.edu>
2023-01-15 20:32:59 -0500

    feature: added dotenv to detect environment variables

--- backend/main.ts
+++ backend/main.ts
@@ -1,17 +1,21 @@
 /* eslint-disable no-console */
 import express, { Express, Request, Response } from 'express';
 import cors from 'cors';
-
+import * as dotenv from 'dotenv';
+import SyllabusRouter from './routes/SyllabusRouter';
 import example from './middleware/example';
 
+dotenv.config();
+
 const app: Express = express();
-const port = 5000;
+const port = process.env.API_PORT || 4000;
 
 app.use(express.json());
 app.use(cors());
+app.use('/api', SyllabusRouter);
 
 app.get('/', example, (req: Request, res: Response) => {
-    res.send('Hello world');
+    res.send('Hello world!!!!');
 });
 
 app.get('/fake', (req, res) => {
