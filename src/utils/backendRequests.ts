150ec7b361735bee541e519eb850d099e9b5fd54
Harrison Kim <kim.harr@northeastern.edu>
2023-02-22 23:13:51 -0500

    fix: reverted back to production backend URL

--- src/utils/backendRequests.ts
+++ src/utils/backendRequests.ts
@@ -9,9 +9,13 @@ const BACKEND_URL = 'https://syllabus-neu-backend.vercel.app/';
 export const uploadToS3Bucket = async (fileToUpload: File): Promise<string> => {
     const formData = new FormData();
     formData.append('pdf-file', fileToUpload);
-    const { data } = await axios.post(`${DEV_URL}/api/s3/objects`, formData, {
-        headers: { 'Content-Type': 'multipart/form-data' },
-    });
+    const { data } = await axios.post(
+        `${BACKEND_URL}/api/s3/objects`,
+        formData,
+        {
+            headers: { 'Content-Type': 'multipart/form-data' },
+        },
+    );
     return data.objectKey;
 };
 
@@ -19,7 +23,7 @@ export const uploadToDynamoDB = async (key: string, formData: Syllabus) => {
     const keyObject = { id: key };
     const params = JSON.stringify(merge(formData, keyObject));
 
-    await axios.post(`${DEV_URL}/api/dynamo/objects`, params, {
+    await axios.post(`${BACKEND_URL}/api/dynamo/objects`, params, {
         headers: { 'Content-Type': 'application/json' },
     });
 };
@@ -34,12 +38,12 @@ export const uploadToS3DynamoPipeline = async (
 
 export const getUnsignedURL = async (key: string) => {
     const { data } = await axios.get(
-        `${DEV_URL}/api/s3/objects/unsignedURL/${key}`,
+        `${BACKEND_URL}/api/s3/objects/unsignedURL/${key}`,
     );
     return data;
 };
 
 export const getDynamoDBItems = async () => {
-    const { data } = await axios.get(`${DEV_URL}/api/dynamo/objects`);
+    const { data } = await axios.get(`${BACKEND_URL}/api/dynamo/objects`);
     return data;
 };
